e61f48282b3bc4e30acc5426c69f8f97ce94adce
Robert Rodini <rrodini@hotmail.com>
2022-08-30 09:58:35 -0400

    Updated Scripts & added savepropertyfiles.sh

--- Scripts/fullballotgen.sh
+++ Scripts/fullballotgen.sh
@@ -6,8 +6,8 @@
 
 VOTER_SERVICES_SPECIMEN="General-2021.pdf"
 VOTER_SERVICES_PAGES_PER_BALLOT=2
-#CONTESTS_FILE="../contests"
-JVM_LOG4J_LEVEL="-Dlog.level=ERROR"
+PRECINCTS_ZONES_CSV="./precincts-zones.csv"
+JVM_LOG4J_LEVEL="-Dlog.level=INFO"
 JVM_LOG4J_CONFIG="-Dlog4j.configurationFile=./resources/log4j-file-config.xml"
 
 if [ -n BALLOTGEN_VERSION ]; then
@@ -17,24 +17,44 @@ else
   exit 0
 fi
 
+## ATTENTION - Voter Services has a flaw in the patterns within General-2021.txt. 
+##             This flaw must be fixed by  manually!
+## extract text from voter service's PDF file.
+#printf '%s \n' "Extracting text from ${VOTER_SERVICES_SPECIMEN}"
+#java -jar ./PDFBOX/pdfbox-app-2.0.25.jar ExtractText "${VOTER_SERVICES_SPECIMEN}"
+
+# run ContestGen to populate the contests directory.
+cd ./contestgen
+#java ${JVM_LOG4J_LEVEL} ${JVM_LOG4J_CONFIG} -jar "contest-gen-${BALLOTGEN_VERSION}-jar-with-dependencies.jar" "../${VOTER_SERVICES_SPECIMEN/.pdf/.txt}" ../contests
+cd ..
+
+
 # run PDFBOX to split voter service's PDF into municipal level PDFs.
 printf '%s\n' "Splitting ${VOTER_SERVICES_SPECIMEN} into municipal PDFs"
 cd ./output
-java -jar ../PDFBOX/pdfbox-app-2.0.25.jar PDFSplit -split $VOTER_SERVICES_PAGES_PER_BALLOT -outputPrefix municipal "../${VOTER_SERVICES_SPECIMEN}"
+#java -jar ../PDFBOX/pdfbox-app-2.0.25.jar PDFSplit -split $VOTER_SERVICES_PAGES_PER_BALLOT -outputPrefix municipal "../${VOTER_SERVICES_SPECIMEN}"
 cd ..
 
 # run a script PDFBOX to get the text extracted from the municipal PDFs.
 printf '%s\n' "Extracting text from municipal PDFs"
-for FILE in ./output/*; do echo "Extracting: $FILE"; java -jar ./PDFBOX/pdfbox-app-2.0.25.jar ExtractText $FILE ; done
+#for FILE in ./output/*; do echo "Extracting: $FILE"; java -jar ./PDFBOX/pdfbox-app-2.0.25.jar ExtractText $FILE ; done
 
 # run BallotNamer to rename files and do some pre-processing.
 printf '%s \n' "Renaming municipal files"
 cd ./ballotnamer
-java ${JVM_LOG4J_LEVEL} ${JVM_LOG4J_CONFIG} -jar "ballot-namer-${BALLOTGEN_VERSION}-jar-with-dependencies.jar" ../output
+#java ${JVM_LOG4J_LEVEL} ${JVM_LOG4J_CONFIG} -jar "ballot-namer-${BALLOTGEN_VERSION}-jar-with-dependencies.jar" ../output
 cd ..
 
 # run BallotGen to generate .docx files for distribution
 printf '%s \n' "Generating municipal docx files"
 cd ballotgen
-java ${JVM_LOG4J_LEVEL} ${JVM_LOG4J_CONFIG} -jar "ballot-gen-${BALLOTGEN_VERSION}-jar-with-dependencies.jar" ../output ../contests
+#java ${JVM_LOG4J_LEVEL} ${JVM_LOG4J_CONFIG} -jar "ballot-gen-${BALLOTGEN_VERSION}-jar-with-dependencies.jar" ../output ../contests
 cd ..
+
+# run BallotZipper to generate .zip files for distribution
+printf '%s \n' "Generating zone .zip files"
+cd ballotzipper
+#java ${JVM_LOG4J_LEVEL} ${JVM_LOG4J_CONFIG} -jar "ballot-zipper-${BALLOTGEN_VERSION}-jar-with-dependencies.jar" "${PRECINCTS_ZONES_CSV}" ../output ../zip
+cd ..
+
+echo "DONE."
